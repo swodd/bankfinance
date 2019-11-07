2491e459b2b742188d83153dc9b3a1f76e368045
swodd <swodd@mail.ru>
2019-11-07 17:31:50 +0200

    add repositories

---
+++ src/main/java/com/bankfinance/management/repositories/RolesRepository.java
@@ -0,0 +1,9 @@
+package com.bankfinance.management.repositories;
+
+import com.bankfinance.management.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RolesRepository extends JpaRepository<Role, Long> {
+}
